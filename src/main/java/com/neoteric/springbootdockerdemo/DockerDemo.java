4579ee6ceb1b33dfbf6726fcafb9a8adeb0e7fed
Anilyadav0827 <anilyadavmekala6@gmail.com>
2024-10-06 10:23:53 +0530

    DockerDemo

---
+++ src/main/java/com/neoteric/springbootdockerdemo/DockerDemo.java
@@ -0,0 +1,14 @@
+package com.neoteric.springbootdockerdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerDemo {
+    @GetMapping(value = "api/hello",produces = "application/json",consumes = "application/json")
+    public String getStatus(){
+        System.out.println("hello world");
+        return "";
+    }
+
+}
