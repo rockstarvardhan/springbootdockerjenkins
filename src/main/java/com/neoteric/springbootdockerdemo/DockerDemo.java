97042709215a7d61ebdb9065ae5f41a2ae4ef810
Anilyadav0827 <anilyadavmekala6@gmail.com>
2024-10-08 13:01:25 +0530

    jenkins

--- src/main/java/com/neoteric/springbootdockerdemo/DockerDemo.java
+++ src/main/java/com/neoteric/springbootdockerdemo/DockerDemo.java
@@ -8,7 +8,7 @@ public class DockerDemo {
     @GetMapping(value = "api/hello",produces = "application/json",consumes = "application/json")
     public String getStatus(){
         System.out.println("hello world");
-        System.out.println("hello");
+        System.out.println("hello nnnnn ");
         return "";
     }
 
